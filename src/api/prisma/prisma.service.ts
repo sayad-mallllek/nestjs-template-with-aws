e9842db20ec44d0647b19c81595c7e8f903248d6
sayad-mallllek <bob@khalilelzein.com>
2023-09-18 22:01:41 +0300

    temporary glob type changes

--- src/api/prisma/prisma.service.ts
+++ src/api/prisma/prisma.service.ts
@@ -1,37 +1,39 @@
-
 import {
-    Injectable,
-    Logger,
-    OnModuleDestroy,
-    OnModuleInit,
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
-    extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy {
-    constructor(private readonly configService: Config.ConfigService) {
-        super({
-            log: [
-                { emit: 'event', level: 'query' },
-                { emit: 'stdout', level: 'info' },
-                { emit: 'stdout', level: 'warn' },
-                { emit: 'stdout', level: 'error' },
-            ],
-            errorFormat: configService.get("") === 'production' ? 'minimal' : 'pretty',
-        });
-    }
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: Config.ConfigService) {
+    configService.get('');
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+      errorFormat:
+        configService.get('') === 'production' ? 'minimal' : 'pretty',
+    });
+  }
 
-    async onModuleInit() {
-        try {
-            await this.$connect();
-        } catch (error) {
-            Logger.error(error);
-        }
+  async onModuleInit() {
+    try {
+      await this.$connect();
+    } catch (error) {
+      Logger.error(error);
     }
+  }
 
-    async onModuleDestroy() {
-        await this.$disconnect();
-    }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,55 +1,79 @@
-import { ConfigService as NestConfigService } from '@nestjs/config';
+import {
+  ConfigGetOptions,
+  ConfigService as NestConfigService,
+  NoInferType,
+  Path,
+  PathValue,
+} from '@nestjs/config';
 
 interface EnvironmentalVariablesType {
-    DATABASE_URL: string;
-    NODE_ENV: string;
-    PORT: number;
-    COGNITO_ACCESS_KEY_ID: string;
-    COGNITO_SECRET_ACCESS_KEY: string;
-    COGNITO_POOL_ID: string;
-    COGNITO_CLIENT_ID: string;
-    COGNITO_REGION: string;
-    COGNITO_KEYS: string;
-    COGNITO_DOMAIN: string;
-    ACCESS_TOKEN_SECRET: string;
-    ACCESS_TOKEN_EXPIRY: string;
-    REFRESH_TOKEN_SECRET: string;
-    REFRESH_TOKEN_EXPIRY: string;
-    MAIL_API_BASE_URL: string;
-    MAIL_API_KEY: string;
-    MAIL_API_SENDER_EMAIL: string;
-    MAIL_API_SENDER_NAME: string;
+  DATABASE_URL: string;
+  NODE_ENV: string;
+  PORT: number;
+  COGNITO_ACCESS_KEY_ID: string;
+  COGNITO_SECRET_ACCESS_KEY: string;
+  COGNITO_POOL_ID: string;
+  COGNITO_CLIENT_ID: string;
+  COGNITO_REGION: string;
+  COGNITO_KEYS: string;
+  COGNITO_DOMAIN: string;
+  ACCESS_TOKEN_SECRET: string;
+  ACCESS_TOKEN_EXPIRY: string;
+  REFRESH_TOKEN_SECRET: string;
+  REFRESH_TOKEN_EXPIRY: string;
+  MAIL_API_BASE_URL: string;
+  MAIL_API_KEY: string;
+  MAIL_API_SENDER_EMAIL: string;
+  MAIL_API_SENDER_NAME: string;
 }
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DATABASE_URL: string;
-            NODE_ENV: string;
-            PORT: number;
-            COGNITO_ACCESS_KEY_ID: string;
-            COGNITO_SECRET_ACCESS_KEY: string;
-            COGNITO_POOL_ID: string;
-            COGNITO_CLIENT_ID: string;
-            COGNITO_REGION: string;
-            COGNITO_KEYS: string;
-            COGNITO_DOMAIN: string;
-            ACCESS_TOKEN_SECRET: string;
-            ACCESS_TOKEN_EXPIRY: string;
-            REFRESH_TOKEN_SECRET: string;
-            REFRESH_TOKEN_EXPIRY: string;
-            MAIL_API_BASE_URL: string;
-            MAIL_API_KEY: string;
-            MAIL_API_SENDER_EMAIL: string;
-            MAIL_API_SENDER_NAME: string;
-        }
+  namespace NodeJS {
+    type ProcessEnv = {
+      DATABASE_URL: string;
+      NODE_ENV: string;
+      PORT: number;
+      COGNITO_ACCESS_KEY_ID: string;
+      COGNITO_SECRET_ACCESS_KEY: string;
+      COGNITO_POOL_ID: string;
+      COGNITO_CLIENT_ID: string;
+      COGNITO_REGION: string;
+      COGNITO_KEYS: string;
+      COGNITO_DOMAIN: string;
+      ACCESS_TOKEN_SECRET: string;
+      ACCESS_TOKEN_EXPIRY: string;
+      REFRESH_TOKEN_SECRET: string;
+      REFRESH_TOKEN_EXPIRY: string;
+      MAIL_API_BASE_URL: string;
+      MAIL_API_KEY: string;
+      MAIL_API_SENDER_EMAIL: string;
+      MAIL_API_SENDER_NAME: string;
+    };
+  }
 
-    }
+  namespace Config {
+    // type ConfigService = NestConfigService<NodeJS.ProcessEnv>;
+    class ConfigService extends NestConfigService<NodeJS.ProcessEnv> {
+      constructor(internalConfig?: Record<string, any>) {
+        super(internalConfig);
+      }
 
-    namespace Config {
-        type ConfigService = NestConfigService<NodeJS.ProcessEnv>;
+      get<T = string | number>(propertyPath: keyof NodeJS.ProcessEnv): T;
+      get<T = NodeJS.ProcessEnv, P extends Path<T> = any, R = PathValue<T, P>>(
+        propertyPath: P,
+        options: ConfigGetOptions,
+      ): R;
+      get<T = string | number>(
+        propertyPath: keyof NodeJS.ProcessEnv,
+        defaultValue: NoInferType<T>,
+      ): T;
+      get<T = NodeJS.ProcessEnv, P extends Path<T> = any, R = PathValue<T, P>>(
+        propertyPath: P,
+        defaultValue: NoInferType<R>,
+        options: ConfigGetOptions,
+      ): Exclude<R, undefined>;
     }
+  }
 }
 
-
-export { };
+export {};
