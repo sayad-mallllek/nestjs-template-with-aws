b58cb1c1f9505309925f559eb83d133588d0d9d8
sayad-mallllek <bob@khalilelzein.com>
2023-12-03 09:53:01 +0200

    general push for aws from main

---
+++ src/integrations/translator/translator.module.ts
@@ -0,0 +1,32 @@
+import { Global, Module } from '@nestjs/common';
+import {
+  AcceptLanguageResolver,
+  CookieResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import * as path from 'path';
+
+import { TranslatorService } from './translator.service';
+
+@Global()
+@Module({
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(require.main.path, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang', 'l'] },
+        AcceptLanguageResolver,
+        CookieResolver,
+      ],
+      typesOutputPath: path.join('src/types/i18n.types.ts'),
+    }),
+  ],
+  providers: [TranslatorService],
+  exports: [TranslatorService],
+})
+export class TranslatorModule {}
